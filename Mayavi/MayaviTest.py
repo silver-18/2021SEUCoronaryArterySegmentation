dbcf5b2b02f96ee3c615c5c1ab86888fac2beed5
silver18 <1020272947@qq.com>
2021-03-25 11:58:30 +0800

    Mayavi

--- Mayavi/MayaviTest.py
+++ Mayavi/MayaviTest.py
@@ -4,12 +4,14 @@ from matplotlib import pyplot as plt
 
 # import tvtk
 
-raw = np.fromfile(file="Data/raw/1.raw", dtype=np.float32)
+raw = np.fromfile(file="Data/image/1.raw", dtype=np.float32)
 label = np.fromfile(file="Data/label/1.raw", dtype=np.float32)
 # shape = [z,y,x]
 shape = np.load("Data/npy/1.npy")
 raw = raw.reshape(shape[0], shape[1], shape[2])
 label = label.reshape(shape[0], shape[1], shape[2])
+raw = np.where(raw > np.mean(raw), 1, 0)
+pass
 # raw.tofile("Data/predict/raw.raw")
 # mean = np.average(raw)
 # heart = np.where(raw > mean, raw, mean)
@@ -20,8 +22,8 @@ label = label.reshape(shape[0], shape[1], shape[2])
 # obj = mlab.contour3d(raw, colormap="autumn", opacity=0.5, name="label")
 # obj.visible = False
 # vol = mlab.pipeline.volume(mlab.pipeline.scalar_field(raw), name='3-d ultrasound ')
-mlab.volume_slice(raw, colormap='gray',
-                  plane_orientation='x_axes', slice_index=0)  # 设定x轴切面
+# mlab.volume_slice(raw, colormap='gray',
+# plane_orientation='x_axes', slice_index=0)  # 设定x轴切面
 # mlab.volume_slice(raw, colormap='gray',
 #                   plane_orientation='y_axes', slice_index=0)  # 设定y轴切面
 # mlab.volume_slice(raw, colormap='gray',
